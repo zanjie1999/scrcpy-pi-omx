657360ab463442fe1115f7ade3a5163116e4bd9c
Sparkle <zanjie1999@yeah.net>
2020-01-11 23:48:28 +0800

    bugfix

--- startscreen.sh
+++ startscreen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-adb push /usr/local/share/scrcpy/scrcpy-server.jar /data/local/tmp/
+adb push /usr/local/share/scrcpy/scrcpy-server /data/local/tmp/scrcpy-server.jar
 adb reverse localabstract:scrcpy tcp:56100
 if [ -x /tmp/streamer ]
  then
@@ -8,7 +8,8 @@ else
    mkfifo /tmp/streamer
    nc -l -p 56100 > /tmp/streamer &
 fi
-omxplayer -o hdmi --no-keys --no-osd --refresh  --advanced=0 --loop --timeout 0 --lavfdopts probesize:250000 --video_queue 1 --avdict rtsp_transport:tcp  /home/pi/streamer &
-sleep 5
+omxplayer -o hdmi --no-keys --no-osd --refresh  --advanced=0 --loop --timeout 0 --lavfdopts probesize:250000 --video_queue 1 --avdict rtsp_transport:tcp  /tmp/streamer &
+sleep 2
 adb shell CLASSPATH=/data/local/tmp/scrcpy-server.jar app_process / com.genymobile.scrcpy.Server 0 8000000 false - false true
+killall nc
 exit 0
